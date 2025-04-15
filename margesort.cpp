9a658be59f39c095ad93437f36103bf946c6645d
sekar243 <kinasekin6602@gmail.com>
2025-04-15 11:31:02 +0700

    step 1

--- margesort.cpp
+++ margesort.cpp
@@ -32,3 +32,12 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+}
