2297ad61788862eb5919100b79974f20c833523e
sekar243 <kinasekin6602@gmail.com>
2025-04-15 11:26:13 +0700

    menambahkan validasi element array

--- margesort.cpp
+++ margesort.cpp
@@ -12,5 +12,23 @@ void input()
     {
         cout << "Masukkan Panjang element array : ";
         cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n--------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n--------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " :" ;
+        cin >> arr[i];
     }
 }
